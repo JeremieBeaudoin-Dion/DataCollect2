94a54cde9ea8a4710ba18edd37cd8f05f4dee5ca
Ariel Kim <Arielekc@gmail.com>
2019-11-14 18:44:14 -0500

    Authentication button implemented, 2 separate types of notification (authentication and recording) added and functional, Authentication reject/accept indicators implemented, python code is commented out due to crashes in the system.

--- app/src/main/java/com/pjinkim/sensors_data_logger/MainActivity.java
+++ app/src/main/java/com/pjinkim/sensors_data_logger/MainActivity.java
@@ -3,7 +3,6 @@ package com.pjinkim.sensors_data_logger;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -13,6 +12,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -25,17 +25,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Timer;
@@ -65,7 +61,9 @@ public class MainActivity extends Activity {
 
 
     private Button mStartStopButton;
+    private Button mAuthenticationButton;
     private TextView mLabelInterfaceTime;
+    private TextView mLabelAuthenticationResult;
     private EditText mUserIdentifier;
     private Timer mInterfaceTimer;
     private int mSecondCounter = 0;
@@ -89,10 +87,11 @@ public class MainActivity extends Activity {
     private DevicePolicyManager mgr = null;
     private ComponentName cn= null;
 
-    //For Notification
+    //For Recording Notification
     private final String CHANNEL_ID = "recording_stop";
     private final int NOTIFICATION_ID = 001;
     private boolean isRecording = false;
+
 
     // Android activity lifecycle states
     @Override
@@ -117,6 +116,7 @@ public class MainActivity extends Activity {
 
         // monitor various sensor measurements
         mLabelInterfaceTime.setText(R.string.ready_title);
+        mLabelAuthenticationResult.setVisibility(View.INVISIBLE);
 
         //Set DevicePolicyManager
         cn = new ComponentName(this, AdminReceiver.class);
@@ -133,18 +133,18 @@ public class MainActivity extends Activity {
         updateConfig();
 
         // Chaquopy: Starts python
-        if (! Python.isStarted()) {
-            Python.start(new AndroidPlatform(this));
-            py = Python.getInstance();
-        } else {
-            py = Python.getInstance();
-        }
+        //if (! Python.isStarted()) {
+        //  Python.start(new AndroidPlatform(this));
+        //py = Python.getInstance();
+        //} else {
+        //    py = Python.getInstance();
+        //}
 
         // Test python integration of SVM
-        float[] accell = {0, 0, 0};
-        float[] gyro = {0, 0, 0};
-        float[] magnet = {0, 0, 0};
-        int value = validateUserWithSVM(accell, gyro, magnet);
+        //float[] accell = {0, 0, 0};
+        //float[] gyro = {0, 0, 0};
+        //float[] magnet = {0, 0, 0};
+        //int value = validateUserWithSVM(accell, gyro, magnet);
     }
 
 
@@ -223,15 +223,18 @@ public class MainActivity extends Activity {
                 // stop interface timer on display
                 mInterfaceTimer.cancel();
                 mLabelInterfaceTime.setText(R.string.ready_title);
+                mLabelAuthenticationResult.setVisibility(View.INVISIBLE);
+
+
             }
             //if device manager isn't active, it will show a prompt to the user explaining what
             //it is and what its used for.
         } else {
-                Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, cn);
-                intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
-                        getString(R.string.permission_explanation));
-                startActivity(intent);
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, cn);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                    getString(R.string.permission_explanation));
+            startActivity(intent);
         }
     }
 
@@ -242,14 +245,22 @@ public class MainActivity extends Activity {
      * output:  0 if this is not user
      *          1 if this is the user
      */
-    private int validateUserWithSVM(float[] accell, float[] gyro, float[] magnet) {
-        // Get hello.py as test object -- calling
-        PyObject helloModule = py.getModule("hello");
-        PyObject svmObject = helloModule.callAttr("SVM");
+    //private int validateUserWithSVM(float[] accell, float[] gyro, float[] magnet) {
+    // Get hello.py as test object -- calling
+    // PyObject helloModule = py.getModule("hello");
+    // PyObject svmObject = helloModule.callAttr("SVM");
 
-        // int isUser = svmObject.callAttr("returnFive").toInt();
-        int isUser = svmObject.callAttr("validate_user", accell, gyro, magnet).toInt();
+    // int isUser = svmObject.callAttr("returnFive").toInt();
+    // int isUser = svmObject.callAttr("validate_user", accell, gyro, magnet).toInt();
 
+    //return isUser;
+    //}
+
+    //Dummy user validator until above method works
+    private int validateUserWithSVM(){
+        final int min = 0;
+        final int max = 1;
+        final int isUser = new Random().nextInt((max - min) + 1) + min;
         return isUser;
     }
 
@@ -281,7 +292,7 @@ public class MainActivity extends Activity {
         mIMUSession.startSession(outputFolder);
         mIsRecording.set(true);
         isRecording = true;
-        displayNotification(mCurrentSubject);
+        displayRecordingNotification(mCurrentSubject);
 
         // update Start/Stop button UI
         runOnUiThread(new Runnable() {
@@ -300,11 +311,124 @@ public class MainActivity extends Activity {
 
                 // stop each session
                 mIMUSession.stopSession();
+                authenticate(0);
                 mIsRecording.set(false);
                 isRecording = false;
 
                 // update screen UI and button
                 showToast("Recording stops!");
+                resetUI();
+            }
+        });
+    }
+
+    public void startStopAuthentication(View view) {
+        //This if branch checks if the policy manager is active.
+        if(mgr.isAdminActive(cn)) {
+
+            if (!mIsRecording.get()) {
+                int value = 5;
+
+                final CountDownTimer countDownTimer = new CountDownTimer(value * 1000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        mAuthenticationButton.setText("Starting in: " + millisUntilFinished / 1000);
+                        mgr.lockNow();
+                    }
+
+                    public void onFinish() {
+                        // mStartStopButton.setText("done!");
+                        // start recording sensor measurements when button is pressed
+                        startAuthentication();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // start interface timer on display
+
+                                mInterfaceTimer = new Timer();
+                                mSecondCounter = 0;
+                                mInterfaceTimer.schedule(new TimerTask() {
+                                    @Override
+                                    public void run() {
+                                        mSecondCounter += 1;
+                                        mLabelInterfaceTime.setText(interfaceIntTime(mSecondCounter));
+                                    }
+                                }, 0, 1000);
+                            }
+                        });
+
+                    }
+                };
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        countDownTimer.start();
+                    }
+                });
+
+
+            } else {
+
+                // stop recording sensor measurements when button is pressed
+                stopAuthentication();
+
+                // stop interface timer on display
+                mInterfaceTimer.cancel();
+                mLabelInterfaceTime.setText(R.string.ready_title);
+                mLabelAuthenticationResult.setVisibility(View.VISIBLE);
+
+
+            }
+            //if device manager isn't active, it will show a prompt to the user explaining what
+            //it is and what its used for.
+        } else {
+            Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, cn);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                    getString(R.string.permission_explanation));
+            startActivity(intent);
+        }
+    }
+
+
+
+
+
+    private void startAuthentication() {
+
+
+        // start each session
+        //mIMUSession.startSession(outputFolder);
+        mIsRecording.set(true);
+        isRecording = true;
+        displayAuthenticationNotification();
+
+        // update Start/Stop button UI
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mAuthenticationButton.setEnabled(true);
+                mStartStopButton.setEnabled(false);
+                mAuthenticationButton.setText(R.string.stop_title);
+            }
+        });
+    }
+
+    protected void stopAuthentication() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                // stop each session
+                //mIMUSession.stopSession();
+                authenticate(validateUserWithSVM());
+                mIsRecording.set(false);
+                isRecording = false;
+
+                // update screen UI and button
+                showToast("Authentication stops!");
                 resetUI();
             }
         });
@@ -361,7 +485,11 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 mStartStopButton.setEnabled(true);
+                mAuthenticationButton.setEnabled(true);
                 mStartStopButton.setText(R.string.start_title);
+                mAuthenticationButton.setText(R.string.authenticate_title);
+
+
             }
         });
     }
@@ -397,7 +525,9 @@ public class MainActivity extends Activity {
 
 
         mStartStopButton = (Button) findViewById(R.id.button_start_stop);
+        mAuthenticationButton = (Button) findViewById(R.id.button_authenticate);
         mLabelInterfaceTime = (TextView) findViewById(R.id.label_interface_time);
+        mLabelAuthenticationResult = (TextView) findViewById(R.id.label_authentication);
         mUserIdentifier = (EditText) findViewById(R.id.user_identifier);
     }
 
@@ -420,14 +550,15 @@ public class MainActivity extends Activity {
         return String.format(Locale.US, "%02d:%02d:%02d", hours, mins, secs);
     }
 
-    //Call this to display our notification to our user
-    private void displayNotification(String userName){
+    //Call this to display recording notification to our user
+    private void displayRecordingNotification(String userName){
         createNotificationChannel();
         //The intent will be responsible of allowing the notification
         //To route us back to the activity and stop the notification from
         //a single click
         Intent stoppingIntent = new Intent(this,MainActivity.class);
-        PendingIntent stoppingPendingIntent = PendingIntent.getActivity(this, 0, stoppingIntent, Intent.FILL_IN_ACTION);
+        stoppingIntent.putExtra("origin","Recording");
+        PendingIntent stoppingPendingIntent = PendingIntent.getActivity(this, 0, stoppingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setContentTitle("Hi, " + userName);
@@ -441,6 +572,38 @@ public class MainActivity extends Activity {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(NOTIFICATION_ID,builder.build());
     }
+
+    //Call this to display recording notification to our user
+    private void displayAuthenticationNotification(){
+        createNotificationChannel();
+        //The intent will be responsible of allowing the notification
+        //To route us back to the activity and stop the notification from
+        //a single click
+        Intent authenticatingIntent = new Intent(this,MainActivity.class);
+        authenticatingIntent.putExtra("origin", "Authentication");
+        PendingIntent authenticatingPendingIntent = PendingIntent.getActivity(this, 1, authenticatingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Hi, phone owner");
+        //Even though our directory naming starts with 0, for subject viewing clarity
+        //the session counter will start at 1 for display purposes only.
+        builder.setContentText("Click me to authenticate");
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setAutoCancel(true);
+        builder.setContentIntent(authenticatingPendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
+    }
+
+
+
+
+
+
+
+
+
 
     //Need to create a NotificationChannel for devices 8.0 and above.
     private void createNotificationChannel(){
@@ -460,8 +623,31 @@ public class MainActivity extends Activity {
     //In future implementation when we authenticate user this might need modification but for the
     //current data collection purposes this fits our criteria
     protected void onNewIntent(Intent intent){
-        if(isRecording){
-            startStopRecording(mStartStopButton);
+        //Extras is used in case the architecture changes and the value is passed before the notification
+        Bundle extras = intent.getExtras();
+        String origin;
+        Log.d("onNewIntent","Entered onNewIntent");
+        if (extras != null){
+            origin = extras.getString("origin");
+            Log.d("origin","origin value of: " + origin);
+            if (origin.equals("Recording")){
+                if(isRecording){
+                    Log.d("RecordingOrigin","Entered Recording origin");
+                    startStopRecording(mStartStopButton);
+                }
+            } else if (origin.equals("Authentication")){
+                startStopAuthentication(mAuthenticationButton);
+            }
+        }
+    }
+
+    protected void authenticate(int result){
+        if (result == 0){
+            mLabelAuthenticationResult.setTextColor(Color.RED);
+            mLabelAuthenticationResult.setText("REJECTED");
+        } else if (result == 1){
+            mLabelAuthenticationResult.setTextColor(Color.GREEN);
+            mLabelAuthenticationResult.setText("ACCEPTED");
         }
     }
 
